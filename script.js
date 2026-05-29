c275328978b13861d02a947d3d26c20f36216411
ConnorXi <steveqi@ConnorQideMacBook-Air.local>
2026-05-29 15:22:01 +0800

    Add pricing plans

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const tryButton = document.querySelector("#tryButton");
+const buyButtons = document.querySelectorAll(".buy-button");
 
 tryButton.addEventListener("click", () => {
   document.querySelector("#tools").scrollIntoView({
     behavior: "smooth",
     block: "start",
+  });
+});
+
+buyButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Demo 网站，暂未接入支付");
   });
 });
